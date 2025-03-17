c40e50a419018a19f66f6f3e0b7a2ca9faef0ed0
bhanuprakash678910 <43405158+bhanuprakash678910@users.noreply.github.com>
2025-03-17 08:23:45 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 resource "aws_instance" "demo" {
   ami           = "ami-04b4f1a9cf54c11d0"
-  instance_type = "t2.micro"
+  instance_type = var.instance_type
 
   tags = {
     Name = "demo-instance"
